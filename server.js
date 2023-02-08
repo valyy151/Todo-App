ff54d724e4e266ecd5754ba296bd4d574a5fad37
valyy151 <marin.valenta1@gmail.com>
2023-02-08 15:20:34 +0100

    Commit before initializing server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV !== 'producion') {
+if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
@@ -15,22 +15,25 @@ app.use(expressLayouts);
 app.use(express.urlencoded({ extended: true }));
 
 const mongoose = require('mongoose');
+
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
+db.on('error', (error) => console.error(error));
 
 const indexRouter = require('./routes/index');
 const aboutRouter = require('./routes/about');
 const loginRouter = require('./routes/login');
 const supportRouter = require('./routes/support');
+const submitRouter = require('./routes/submit');
 
 app.use('/index', indexRouter);
 app.use('/about', aboutRouter);
 app.use('/login', loginRouter);
 app.use('/support', supportRouter);
+app.use('/submit', submitRouter);
 
 app.get('/', (req, res) => {
   res.redirect('/index');
